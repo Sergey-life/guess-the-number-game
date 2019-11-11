bb6dea89abf7c3e4c80e271729a6ec49ca6408f3
Serhiy Kharchenko <uetsmedia@UETSMedia-2.local>
2019-11-11 15:14:05 +0200

    commit home_1_years

--- home_2.php
+++ home_2.php
@@ -1,3 +1,9 @@
 <?php
-echo implode(', ', range(1000, 3000, 4));
+//echo implode(', ', range(1000, 3000, 4));
+
+for ($i = 1000; $i < 3000; $i++) {
+    if ($i % 4 == 0) {
+        echo $i . ' ';
+    }
+}
 
