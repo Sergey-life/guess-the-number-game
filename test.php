b227e4aa077f0c84fcf67518f648c2eaf8c8191a
Serhiy Kharchenko <uetsmedia@UETSMedia-2.local>
2019-11-11 15:00:33 +0200

    commit secret_number

--- test.php
+++ test.php
@@ -1,12 +1,24 @@
 <?php
-$secret_number = 12;
 
-if ($secret_number < 10) {
-    echo 'Введите число больше!' . "\n";
-} elseif ($secret_number > 10) {
-    echo 'Введите число меньше!' . "\n";
+echo 'Угадай число от 1 до 100';
+
+for ($i = 0; $i < 1000; $i++) {
+    $line = readline( "\n");
+    readline_add_history($line);
+
+    $secret_number = rand(1, 100);
+
+    if ($line < $secret_number) {
+        echo 'Введите большее число!';
+    } elseif ($line > $secret_number) {
+        echo 'Введите меньшее число!';
+    } elseif ($line == $secret_number) {
+        echo 'Вы угадали!!! ' . "\n";
+        echo 'Количество сделаных попыток: ' . ++$i . "\n";
+        exit();
+    }
+
 }
 
-elseif ( $secret_number == 10) {
-    echo 'Вы выиграли!!!' . "\n";
-}
+
+
