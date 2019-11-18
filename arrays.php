3b887603276a6b9451e0cec4277a8aa744f1a9fd
Serhiy Kharchenko <uetsmedia@UETSMedia-2.local>
2019-11-18 12:59:01 +0200

    commit

--- arrays.php
+++ arrays.php
@@ -1,1 +1,30 @@
 <?php
+
+$limit = readline("Enter limit:  ");
+$sequence = generate_fib_sequence($limit);
+show_seqence($sequence);
+
+function generate_fib_sequence($limit)
+{
+    $a = 1;
+    $b = 0;
+    $i = 0;
+    while (true){
+
+        $v = $a + $b;
+        if ($v >= $limit) {
+            break;
+        }
+        $i++;
+        echo '> Position ' . $i . ': ' . $array[] = $v . "\n";
+
+        $b = $a;
+        $a = $v;
+    }
+
+}
+
+function show_seqence($sequence)
+{
+//   echo $sequence;
+}
